f6f9083a7c9fbe098bac2c7da87637f456b4c64a
julesrx <dev@julesrx.fr>
2023-11-14 14:14:09 +0100

    Router

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { authLoader, callbackRoute } from "~/auth";
+import App from "~/App";
+import Index from "./routes/Index";
 import Queue from "routes/Queue";
 import Albums from "routes/Albums";
-import App from "~/App";
 
 const router = createBrowserRouter([
+  callbackRoute,
   {
     path: "/",
     loader: authLoader,
     element: <App />,
     children: [
-      { index: true, element: <p>this is home</p> },
+      { index: true, element: <Index /> },
       { path: "queue", element: <Queue /> },
       { path: "albums", element: <Albums /> },
     ],
   },
-  callbackRoute,
 ]);
 
 export default router;
